2656e650c7ecbb7aeff98e265d7070cea65bda7e
SylvainLeDev <dev.tess.sylvain@gmail.com>
2023-11-22 22:00:14 +0100

    init vite + react + tailwin

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import TestLunarUi from './components/LunarUI'
+
+function App() {
+
+  return (
+    <>
+     <TestLunarUi/>
+    </>
+  )
+}
+
+export default App
